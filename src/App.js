66f4407f52b85fb3443c14d8edf58a8db1e77b55
palel <32599663+palel@users.noreply.github.com>
2021-06-10 11:48:26 +0200

    display list of currencies from array using map

--- src/App.js
+++ src/App.js
@@ -8,17 +8,40 @@ import Fieldset from "./Fieldset";
 import Form from "./Form";
 import Container from "./Container";
 
+const currencies = [
+  { 
+    id: 1,
+    name: "Złoty", 
+    image:"https://i.postimg.cc/6QY6tWdx/pln.png", 
+    altText:"flaga Polski",
+    rate: 1 
+  },
+  { 
+    id: 2, 
+    name: "Euro", 
+    image: "https://i.postimg.cc/ht0QQq9p/euro.jpg",
+    altText: "flaga Europy",
+    rate: 4.54 },
+  { 
+    id: 3,
+    name: "Dolar",
+    image: "https://i.postimg.cc/sXbMCfrD/usd.png",
+    altText: "flaga Stanów Zjednoczonych",
+    rate: 3.79 },
+];
+
 function App() {
+
   return (
     <Container>
       <Header title="Internetowy kantor walut" />
       <Form>
         <Fieldset title="Co sprzedajesz:">
-          <Currency />
+          <Currency currencies={currencies}/>
           <Prize title="Kwota:" extraContent={<Input />} />
         </Fieldset>
         <Fieldset title="Co kupujesz:">
-          <Currency />
+          <Currency currencies={currencies}/>
           <Prize title="Do wypłaty:" extraContent={<Output />} />
         </Fieldset>
         <Buttons />
--- src/Currency/index.js
+++ src/Currency/index.js
@@ -1,40 +1,20 @@
 import "./style.css";
 
-const Currency = () => (
+const Currency = ({ currencies }) => (
   <ul className="currency">
-    <li className="currency__item">
-      <label className="currency__label">
-        <input type="radio" name="sell" value="zloty" className="currency__input" required />
-        <span className="currency__radioText">Złotówki</span>
-        <img
-          className="currency__image"
-          src="https://i.postimg.cc/6QY6tWdx/pln.png"
-          alt="flaga Polski"
-        />
-      </label>
-    </li>
-    <li className="currency__item">
-      <label className="currency__label">
-        <input type="radio" name="sell" value="euro" className="currency__input" />
-        <span className="currency__radioText">Euro</span>
-        <img
-          className="currency__image"
-          src="https://i.postimg.cc/ht0QQq9p/euro.jpg"
-          alt="flaga Europy"
-        />
-      </label>
-    </li>
-    <li className="currency__item">
-      <label className="currency__label">
-        <input type="radio" name="sell" value="dolar" className="currency__input" />
-        <span className="currency__radioText">Dolar</span>
-        <img
-          className="currency__image"
-          src="https://i.postimg.cc/sXbMCfrD/usd.png"
-          alt="flaga Stanów Zjednoczonych"
-        />
-      </label>
-    </li>
+    {currencies.map(currency => (
+      <li key={currency.id} className="currency__item">
+        <label className="currency__label">
+          <input type="radio" name="sell" value={currency.name} className="currency__input" required />
+          <span className="currency__radioText">{currency.name}</span>
+          <img
+            className="currency__image"
+            src={currency.image}
+            alt={currency.altText}
+          />
+        </label>
+      </li>
+    ))}
   </ul>
 );
 
