dd201073a5836fa316741e90066cf2e169ca966a
palel <32599663+palel@users.noreply.github.com>
2021-06-11 17:41:03 +0200

    currencies array in other file

---
+++ src/Currencies/index.js
@@ -0,0 +1,25 @@
+const currencies = [
+    {
+      id: 1,
+      name: "Złoty",
+      image: "https://i.postimg.cc/6QY6tWdx/pln.png",
+      altText: "flaga Polski",
+      rate: 1
+    },
+    {
+      id: 2,
+      name: "Euro",
+      image: "https://i.postimg.cc/ht0QQq9p/euro.jpg",
+      altText: "flaga Europy",
+      rate: 4.54
+    },
+    {
+      id: 3,
+      name: "Dolar",
+      image: "https://i.postimg.cc/sXbMCfrD/usd.png",
+      altText: "flaga Stanów Zjednoczonych",
+      rate: 3.79
+    },
+  ];
+
+export default currencies;
