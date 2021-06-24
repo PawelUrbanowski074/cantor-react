03dd2f044325663560dbe3b9411f7dd6920b6c6b
palel <32599663+palel@users.noreply.github.com>
2021-06-24 23:21:43 +0200

    styled components for Header

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,7 +1,9 @@
-.header {
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
     padding: 0 30px 0px 30px;
     text-align: center;
     color: #ddffff;
     background-color: darkslategray;
     border-radius: 10px;
-}
+`;
