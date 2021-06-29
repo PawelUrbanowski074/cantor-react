9b49afc17957a722a7e92fb0713c6c9dc955ac01
palel <32599663+palel@users.noreply.github.com>
2021-06-29 23:37:01 +0200

    global style with using styled components

---
+++ src/globalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./images/background.png";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Lato', sans-serif;
+        line-height: 1.5;
+        background-color: darkslategray;
+        background-image: url("${background}");
+        background-position: center;
+    }
+`;
